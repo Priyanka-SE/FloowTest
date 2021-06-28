169a06118c42a35b8355b8011edb06507d525c7f
Priyanka-SE <66633525+Priyanka-SE@users.noreply.github.com>
2021-06-28 19:57:31 +0100

    Adding test file

---
+++ DriverDetails/src/test/java/com/floow/driverdetails/DriverDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floow.driverdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
