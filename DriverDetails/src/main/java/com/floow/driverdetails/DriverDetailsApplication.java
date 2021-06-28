63007ae1a0f5098fda284a69b355298eaa67056b
Priyanka-SE <66633525+Priyanka-SE@users.noreply.github.com>
2021-06-28 19:49:14 +0100

    Adding SpringBoot Application file

---
+++ DriverDetails/src/main/java/com/floow/driverdetails/DriverDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.floow.driverdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverDetailsApplication.class, args);
+	}
+
+}
